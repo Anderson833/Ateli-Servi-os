26a17e7bb9482135fd07246e46bd1594418aeae2
Anderson <anderson21fonseca@gamil.com>
2023-04-25 18:05:35 -0300

    Feito varias funções especificas, relacionadas a id do usuário,descrição,nome do cliente e entre as datas. E funções que emgloba umas com as outras e todas juntas.

--- src/main/java/br/edu/ifrn/atelie/Repository/ServicosRepository.java
+++ src/main/java/br/edu/ifrn/atelie/Repository/ServicosRepository.java
@@ -28,6 +28,80 @@ public interface ServicosRepository extends JpaRepository<Servicos, Integer>{
      	@Query("SELECT x FROM Servicos x WHERE x.nome like %?1%")
      	List<Servicos> buscaServicos(@Param("nome")String nome);
      	
+     	/**
+     	 * Método para lista os servicos pelo id do usuário e descrição
+     	 * @param usuario
+     	 * @param descricao
+     	 * @return Todos dados conforme  id, descrição passados nos parâmetros
+     	 */
+     	@Query("SELECT x FROM Servicos x WHERE x.usuario =?1 and x.descricao like %?2%")
+     	List<Servicos> listaServicosPeloIdUsuarioDescricao(@Param("usuario")Usuario usuario,@Param("descricao")String descricao);
+     	
+     	/**
+     	 * Método para soma todos servicos pelo id do usuário e descrição 
+     	 * @param usuario
+     	 * @param descricao
+     	 * @return O total de toda descrição
+     	 */
+     	@Query("SELECT SUM(x.valorTotal) FROM Servicos x WHERE x.usuario =?1 and x.descricao like %?2%")
+     	Double somaServicosPeloIdUsuarioDescricao(@Param("usuario")Usuario usuario,@Param("descricao")String descricao);
+     	
+    	/**
+    	 * Método para lista os serviços pelo id do usuário,descrição e nome do cliente
+    	 * @param usuario
+    	 * @param descricao
+    	 * @param nome
+    	 * @return Todos dados conforme o id do usuário, descrição e nome do cliente passados nos parâmetro
+    	 */
+     	@Query("SELECT x FROM Servicos x WHERE x.usuario =?1 and x.descricao like %?2% and x.nome like %?3%")
+     	List<Servicos> listaServicosPeloIdUsuarioDescricaoCliente(@Param("usuario")Usuario usuario,@Param("descricao")String descricao,@Param("nome")String nome);
+     	
+     	/**
+     	 * Método para soma os serviços pelo id do usuário,descrição e nome do cliente
+     	 * @param usuario
+     	 * @param descricao
+     	 * @param nome
+     	 * @return O total dos serviços conforme os dados passados nos parâmetros
+     	 */
+     	@Query("SELECT SUM(x.valorTotal) FROM Servicos x WHERE x.usuario =?1 and x.descricao like %?2% and x.nome like %?3%")
+     	Double somaServicosPeloIdUsuarioDescricaoCliente(@Param("usuario")Usuario usuario,@Param("descricao")String descricao,@Param("nome")String nome);
+     	
+     	/**
+     	 * Método para lista os serviços pelo id usuário,descricao, nome do cliente e entre as datas
+     	 * @param usuario
+     	 * @param descricao
+     	 * @param nome
+     	 * @param dataInicio
+     	 * @param datafinal
+     	 * @return Todos dados conforme os dados passados nos parâmetros
+     	 */
+     	@Query("SELECT x FROM Servicos x WHERE x.usuario =?1 and x.descricao like %?2% and x.nome like %?3% and x.data >=?4 and x.data<=?5")
+     	List<Servicos> listaServicosPeloIdUsuarioDescricaoClienteDatas(@Param("usuario")Usuario usuario,@Param("descricao")String descricao,@Param("nome")String nome,
+     			@Param("dataInicio")String dataInicio,@Param("datafinal")String datafinal);
+     	
+     	/**
+     	 * Método para soma todos serviços por id do usuário,descrição,cliente e entre as datas
+     	 * @param usuario
+     	 * @param descricao
+     	 * @param nome
+     	 * @param dataInicio
+     	 * @param datafinal
+     	 * @return Total dos serviços conforme os dados passados nos parâmetros
+     	 */
+    	@Query("SELECT SUM(x.valorTotal) FROM Servicos x WHERE x.usuario =?1 and x.descricao like %?2% and x.nome like %?3% and x.data >=?4 and x.data<=?5")
+     	Double somaServicosPeloIdUsuarioDescricaoClienteDatas(@Param("usuario")Usuario usuario,@Param("descricao")String descricao,@Param("nome")String nome,
+     			@Param("dataInicio")String dataInicio,@Param("datafinal")String datafinal);
+     	
+     	
+       /**
+        * Método para lista todos serviços pelo id do usuário, nome do cliente
+        * @param nome
+        * @param usuario
+        * @return Todos dados conforme os dados que foram passados nos parâmetros
+        */
+     	@Query("SELECT x FROM Servicos x WHERE x.usuario =?1 and x.nome like %?2%")
+     	List<Servicos> listaServicosPeloIdUsuarioNomeCliente(@Param("usuario")Usuario usuario,@Param("nome")String nome);
+     	
         /**
          * Método para somar os serviços pelo id do usuário e nome do cliente
          * @param usuario
@@ -35,31 +109,61 @@ public interface ServicosRepository extends JpaRepository<Servicos, Integer>{
          * @return O total de toda coluna valorTotal no banco de dados 
          */
      	@Query("SELECT SUM(x.valorTotal) FROM Servicos x WHERE x.usuario =?1 and x.nome like %?2%")
-     	Double somandoPeloNomeDoCliente(@Param("usuario")Usuario usuario,@Param("nome")String nome);
+     	Double somandoServicosPeloIdUsuarioNomeDoCliente(@Param("usuario")Usuario usuario,@Param("nome")String nome);
      	
      	
-     // lista todos serviços pela datas de inicio e final
-     /*	@Query("SELECT x FROM Servicos x WHERE x.data > ?1 and x.data <?2")
-     	List<Servicos> listaServicosPelasDatas(@Param("dataTermino")String dataI,@Param("dataTermino")String data);
-     	*/
-     // lista todos serviços pela datas de inicio e final
+     	/**
+     	 * Método para Lista todos serviços com id do usuário, nome do cliente e entre as datas
+     	 * @param us
+     	 * @param nome
+     	 * @param dataI
+     	 * @param dataF
+     	 * @return Uma lista de registros
+     	 */
+    	@Query("SELECT x FROM Servicos x WHERE x.usuario =?1 and x.nome like %?2% and x.data >= ?3 and x.data <=?4")
+     	List<Servicos> listaServicosPeloIdUsuarioNomeClienteDatas(@Param("usuario")Usuario us,@Param("nome")String nome,@Param("data")String dataI,@Param("data")String dataF);
+     	
+     	
+     /**
+      * Método para soma todos serviços pelo id do usuário, nome do cliente e entre as datas
+      * @param us
+      * @param nome
+      * @param dataI
+      * @param dataF
+      * @return A soma de toda coluna valorTotal 
+      */
+     	@Query("SELECT SUM(x.valorTotal) FROM Servicos x WHERE x.usuario =?1 and x.nome like %?2% and x.data >= ?3 and x.data <=?4")
+     	Double SomaServicosPeloIdUsuarioNomeClienteDatas(@Param("usuario")Usuario us,@Param("nome")String nome,@Param("data")String dataI,@Param("data")String dataF);
+     	
+
+        /**
+         * Método para lista todos serviços entre as datas e id do usuário
+         * @param dataI
+         * @param data
+         * @param us
+         * @return Todos os dados conforme os dados passado nos parametros
+         */
      	@Query("SELECT x FROM Servicos x WHERE x.data >= ?1 and x.data <=?2 and x.usuario = ?3")
-     	List<Servicos> listaServicosPelasDatasIdUsuario(@Param("dataTermino")String dataI,@Param("dataTermino")String data,
+     	List<Servicos> listaServicosPelasDatasIdUsuario(@Param("data")String dataI,@Param("data")String data,
      			@Param("usuario")Usuario us);
      	
      	
-     	 // Soma o total de todos  serviços pela datas de inicio e final e id do usuário
+     	 /**
+     	  * Método para soma o total de todos  serviços pela datas de inicio e final e id do usuário
+     	  * @param dataI
+     	  * @param dataf
+     	  * @param us
+     	  * @return Total de todos serviços conforme os dados passados nos parâmetros
+     	  */
      	@Query("SELECT SUM(x.valorTotal) FROM Servicos x WHERE x.data >= ?1 and x.data <=?2 and x.usuario = ?3")
-     	Double  somamdoPorDatasIdUsuario(@Param("dataTermino")String dataI,@Param("dataTermino")String dataf,
+     	Double  somamdoPorDatasIdUsuario(@Param("data")String dataI,@Param("data")String dataf,
      			@Param("usuario")Usuario us);
-     	
-     	 // Soma o total de todos  serviços pela datas de inicio e final e id do usuário
-     /*	@Query("SELECT SUM(x.valorTotal) FROM Servicos x WHERE x.dataTermino > ?1 and x.dataTermino <?2")
-     	Double  somamdoPorDatas(@Param("dataTermino")String dataI,@Param("dataTermino")String dataf);
-     			*/
      			
-     	
-     	// listando todos servicos pelo id do usuário
+     	/**
+     	 * Método para lista todos serviços pelo id do usuário 
+     	 * @param us
+     	 * @return Todos os dados dos serviços
+     	 */
     	@Query("SELECT x FROM Servicos x WHERE x.usuario = ?1")
      	List<Servicos> listaServicosPeloId(@Param("usuario")Usuario us);
     	
